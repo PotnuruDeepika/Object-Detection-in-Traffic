d2fb93b4db4c8b418029b891a781cfe7972fc9b1
POTNURU DEEPIKA <71198147+PotnuruDeepika@users.noreply.github.com>
2022-07-12 05:49:35 +0530

    Add files via upload

---
+++ Maximum no. of objects in video.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+from numpy.lib.polynomial import poly
+import cv2
+from glob import glob
+l=[]
+#loading img
+#path=glob('D:/DEEPIKA/TMG/Videos/traffic1/*.png')
+#for img in path:
+imagePath='D:/DEEPIKA/TMG/Videos/traffic/*.png'
+#glob=glob(imagePath)
+images=[cv2.imread(image) for image in glob(imagePath)]
+for img in images:
+    #img = cv2.imread(i)
+    img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# img1=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+#     plt.figure(figsize=(10,10))
+#     plt.axis("off")
+#     plt.imshow(img1)
+    #plt.show()
+
+    #creating boxes
+    box,label,count=cv.detect_common_objects(img)
+    output=draw_bbox(img,box,label,count)
+
+    output=cv2.cvtColor(output,cv2.COLOR_BGR2RGB)
+    # plt.figure(figsize=(10,10))
+    # plt.axis("off")
+    # plt.imshow(output)
+    #plt.show()
+        #count object in the img
+    #print("Number of object:",len(label))
+    l.append((len(label)))
+print('Max object:',max(l))
+
+
